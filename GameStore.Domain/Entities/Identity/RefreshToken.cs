15a712d282c7d6224a6043c23622782b409de290
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-10-01 00:00:52 +0500

    save

---
+++ GameStore.Domain/Entities/Identity/RefreshToken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Domain.Entities.Identity
+{
+    public class RefreshToken
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public string JwtId { get; set; }
+        public bool IsRevoked { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateExpire { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
