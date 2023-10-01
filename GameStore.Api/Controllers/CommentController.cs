708e8037bb6f1d8c48a498df1d59ba283a089b1a
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-10-01 18:51:53 +0500

    save

--- GameStore.Api/Controllers/CommentController.cs
+++ GameStore.Api/Controllers/CommentController.cs
@@ -1,6 +1,48 @@
-﻿namespace GameStore.WebApi.Controllers
+﻿using GameStore.Application.UseCases.CartItems.Response;
+using GameStore.Application.UseCases.Comments.Commands.CreateComment;
+using GameStore.Application.UseCases.Comments.Commands.DeleteComment;
+using GameStore.Application.UseCases.Comments.Commands.UpdateComment;
+using GameStore.Application.UseCases.Comments.Queries.GetAllComments;
+using GameStore.Application.UseCases.Comments.Queries.GetCommentById;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.WebApi.Controllers
 {
-    public class CommentController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ApiBaseController
     {
+        [HttpPost("[action]")]
+        public async ValueTask<int> CreateComment(CreateCommentCommand command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        [HttpGet("[action]")]
+        public async ValueTask<CommentResponse> GetCommentById(int Id)
+        {
+            return await _mediator.Send(new GetCommentByIdQuery(Id));
+        }
+
+        [HttpGet("[action]")]
+        public async ValueTask<IEnumerable<CommentResponse>> GetAllComments()
+        {
+            return await _mediator.Send(new GetAllCommentsQuery());
+        }
+
+        [HttpPut("[action]")]
+        public async ValueTask<IActionResult> UpdateComment(UpdateCommentCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("[action]")]
+        public async ValueTask<IActionResult> DeleteComment(DeleteCommentCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
     }
+
 }
