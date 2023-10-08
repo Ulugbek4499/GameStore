b3df035eb5d68091ae2496e9ab6b7b4cc7b94a45
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-10-08 10:41:16 +0500

    save

--- GameStore.Api/ConfigureServices.cs
+++ GameStore.Api/ConfigureServices.cs
@@ -10,7 +10,6 @@ public static class ConfigureServices
 {
     public static IServiceCollection AddApi(this IServiceCollection services, IConfiguration configuration)
     {
-        //services.AddTransient<IUpdateHandler, UpdateHandler>();
         services.AddScoped<IApplicationUser, CurrentUser>();
         services.AddControllers().AddJsonOptions(x =>
         {
