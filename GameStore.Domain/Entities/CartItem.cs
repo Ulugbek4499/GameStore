69112ba0b901c4063f4ac04e827e5c35b6b8b631
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-10-12 18:42:18 +0500

    save

--- GameStore.Domain/Entities/CartItem.cs
+++ GameStore.Domain/Entities/CartItem.cs
@@ -9,11 +9,13 @@ namespace GameStore.Domain.Entities.Identity
 {
     public class CartItem:BaseAuditableEntity
     {
-        public int CardId { get; set; }
-        public Cart Cart { get; set; }
         public int Count { get; set; }
+        
+        public int CardId { get; set; }
+        public virtual Cart? Cart { get; set; }
+        
         public int GameId { get; set; }
-        public Game Game { get; set; }
+        public virtual Game? Game { get; set; }
 
     }
 }
