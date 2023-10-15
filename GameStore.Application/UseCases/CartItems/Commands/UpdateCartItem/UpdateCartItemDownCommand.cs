df37712c97e2b423c430ef946cc362d50fca33da
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-10-15 13:41:05 +0500

    save

---
+++ GameStore.Application/UseCases/CartItems/Commands/UpdateCartItem/UpdateCartItemDownCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using GameStore.Application.Common.Interfaces;
+using GameStore.Domain.Entities.Identity;
+using MediatR;
+
+namespace GameStore.Application.UseCases.CartItems.Commands.UpdateCartItem
+{
+    public class UpdateCartItemDownCommand : IRequest
+    {
+        public int Id { get; set; }
+        public int Count { get; set; }=1;
+    }
+
+    public class UpdateCartItemDownCommandHandler : IRequestHandler<UpdateCartItemDownCommand>
+    {
+        private readonly IMapper _mapper;
+        private readonly IApplicationDbContext _context;
+
+        public UpdateCartItemDownCommandHandler(IMapper mapper, IApplicationDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task Handle(UpdateCartItemDownCommand request, CancellationToken cancellationToken)
+        {
+            CartItem? cartItem = await _context.CartItems.FindAsync(request.Id);
+            cartItem.Count -= request.Count;
+
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ GameStore.Application/UseCases/CartItems/Commands/UpdateCartItem/UpdateCartItemUpCommand.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using GameStore.Application.Common.Interfaces;
+using GameStore.Domain.Entities.Identity;
+using MediatR;
+
+namespace GameStore.Application.UseCases.CartItems.Commands.UpdateCartItem
+{
+
+    public class UpdateCartItemUpCommand : IRequest
+    {
+        public int Id { get; set; }
+        public int Count { get; set; }=1;
+    }
+
+    public class UpdateCartItemUpCommandHandler : IRequestHandler<UpdateCartItemUpCommand>
+    {
+        private readonly IMapper _mapper;
+        private readonly IApplicationDbContext _context;
+
+        public UpdateCartItemUpCommandHandler(IMapper mapper, IApplicationDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task Handle(UpdateCartItemUpCommand request, CancellationToken cancellationToken)
+        {
+            CartItem? cartItem = await _context.CartItems.FindAsync(request.Id);
+            cartItem.Count += request.Count;
+
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
