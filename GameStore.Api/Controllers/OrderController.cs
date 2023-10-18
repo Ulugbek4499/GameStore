c4f77bdc7191df3decc37d88bf1d5154d5c45064
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-10-18 16:34:09 +0500

    save

--- GameStore.Api/Controllers/OrderController.cs
+++ GameStore.Api/Controllers/OrderController.cs
@@ -1,9 +1,5 @@
 ﻿using GameStore.Application.UseCases.Orders;
 using GameStore.Application.UseCases.Orders.Commands.CreateOrder;
-using GameStore.Application.UseCases.Orders.Commands.DeleteOrder;
-using GameStore.Application.UseCases.Orders.Commands.UpdateOrder;
-using GameStore.Application.UseCases.Orders.Queries.GetAllOrders;
-using GameStore.Application.UseCases.Orders.Queries.GetOrderById;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GameStore.WebApi.Controllers
@@ -19,30 +15,5 @@ namespace GameStore.WebApi.Controllers
         }
 
 
-        [HttpGet("[action]")]
-        public async ValueTask<OrderResponse> GetOrderById(int Id)
-        {
-            return await _mediator.Send(new GetOrderByIdQuery(Id));
-        }
-
-        [HttpGet("[action]")]
-        public async ValueTask<IEnumerable<OrderResponse>> GetAllOrders()
-        {
-            return await _mediator.Send(new GetAllOrdersQuery());
-        }
-
-        [HttpPut("[action]")]
-        public async ValueTask<IActionResult> UpdateOrder(UpdateOrderCommand command)
-        {
-            await _mediator.Send(command);
-            return NoContent();
-        }
-
-        [HttpDelete("[action]")]
-        public async ValueTask<IActionResult> DeleteOrder(DeleteOrderCommand command)
-        {
-            await _mediator.Send(command);
-            return NoContent();
-        }
     }
 }
