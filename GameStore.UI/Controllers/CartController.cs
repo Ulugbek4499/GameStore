fd61a76553cb9e882c30fd0c96e1a2b0cea7b007
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-10-27 13:51:44 +0500

    save

--- GameStore.UI/Controllers/CartController.cs
+++ GameStore.UI/Controllers/CartController.cs
@@ -1,4 +1,8 @@
-﻿using GameStore.Application.UseCases.Carts.Queries.GetCartByUserId;
+﻿using CartItemStore.Application.UseCases.CartItems.Commands.DeleteCartItem;
+using GameStore.Application.UseCases.CartItems.Commands.CreateCartItem;
+using GameStore.Application.UseCases.CartItems.Commands.UpdateCartItem;
+using GameStore.Application.UseCases.CartItems.Queries.GetAllCartItems;
+using GameStore.Application.UseCases.Carts.Queries.GetCartByUserId;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GameStore.UI.Controllers
@@ -14,7 +18,7 @@ namespace GameStore.UI.Controllers
             return View("ViewCart", Cart);
         }
 
-        // [HttpGet("[action]")]
+        [HttpGet("[action]")]
         public async ValueTask<IActionResult> GetCountOfItems(string userId)
         {
             var cart = await Mediator.Send(new GetCartByUserIdQuery(userId));
@@ -26,6 +30,54 @@ namespace GameStore.UI.Controllers
             }
 
             return Json(count);
+        }
+
+        //Cart Item
+        [HttpPost("[action]")]
+        public async ValueTask<IActionResult> CreateCartItem([FromForm] CreateCartItemCommand CartItem)
+        {
+            await Mediator.Send(CartItem);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet("[action]")]
+        public async ValueTask<IActionResult> GetAllCartItems()
+        {
+            var CartItems = await Mediator.Send(new GetAllCartItemsQuery());
+
+            return View(CartItems);
+        }
+
+        [HttpGet("[action]")]
+        public async ValueTask<IActionResult> UpdateCartItemUp(int Id)
+        {
+            var cart = await Mediator.Send(new GetCartByCartItemIdQuery(Id));
+
+            await Mediator.Send(new UpdateCartItemUpCommand(Id));
+
+            return View("ViewCart", cart);
+        }
+
+        [HttpGet("[action]")]
+        public async ValueTask<IActionResult> UpdateCartItemDown(int Id)
+        {
+            var cart = await Mediator.Send(new GetCartByCartItemIdQuery(Id));
+
+            await Mediator.Send(new UpdateCartItemDownCommand(Id));
+
+            return View("ViewCart", cart);
+        }
+
+        [HttpGet("[action]")]
+        public async ValueTask<IActionResult> DeleteCartItem(int Id)
+        {
+            var cart = await Mediator.Send(new GetCartByCartItemIdQuery(Id));
+
+            await Mediator.Send(new DeleteCartItemCommand(Id));
+
+
+            return View("ViewCart", cart);
         }
     }
 }
