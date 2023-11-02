4a784eea5675fca8e89909a3ad7a86761c81a582
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-11-02 11:32:04 +0500

    save

--- GameStore.Application/Common/Models/AuthResultModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameStore.Application.Models
-{
-    public class AuthResultModel
-    {
-        public string Token { get; set; }
-        public string RefreshToken { get; set; }
-        public DateTime ExpiresAt { get; set; }
-    }
-}
