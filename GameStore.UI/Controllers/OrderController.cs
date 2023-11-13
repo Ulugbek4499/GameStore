8838679980e3ab273012bb44a27178cd69a8dd3a
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-11-14 01:29:12 +0500

    save

--- GameStore.UI/Controllers/OrderController.cs
+++ GameStore.UI/Controllers/OrderController.cs
@@ -24,30 +24,18 @@ namespace GameStore.UI.Controllers
         {
             Order.UserId = _applicationUser.Id;
 
-            // Assuming Mediator.Send(Order) returns an int representing the order ID
             int orderId = await Mediator.Send(Order);
 
             if (orderId > 0)
             {
-                // Display success message on the same page
                 TempData["SuccessMessage"] = "Order successfully created!";
                 return RedirectToAction("Index", "Home");
             }
             else
             {
-                // Handle the case where order creation failed
                 ViewData["ErrorMessage"] = "Failed to create the order.";
                 return View("CreateOrder");
             }
         }
     }
 }
-
-/*        [HttpPost("[action]")]
-        public async ValueTask<IActionResult> CreateOrder([FromForm] CreateOrderCommand Order)
-        {
-            Order.UserId = _applicationUser.Id;
-            await Mediator.Send(Order);
-
-            return RedirectToAction("Index", "Home");
-        }*/
