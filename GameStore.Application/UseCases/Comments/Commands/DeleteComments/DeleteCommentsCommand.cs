af5dd3317cc1e198d7fa49656f30afb00cac17b1
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-11-23 00:43:20 +0500

    save

---
+++ GameStore.Application/UseCases/Comments/Commands/DeleteComments/DeleteCommentsCommand.cs
@@ -0,0 +1,29 @@
+﻿using GameStore.Application.Common.Interfaces;
+using GameStore.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Application.UseCases.Comments.Commands.DeleteComments
+{
+    public record DeleteCommentsCommand : IRequest;
+    public class DeleteCommentsCommandHandler : IRequestHandler<DeleteCommentsCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteCommentsCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(DeleteCommentsCommand request, CancellationToken cancellationToken)
+        {
+            List<Comment> commentsToDelete = await _context.Comments
+                    .Where(comment => comment.IsDeleted)
+                    .ToListAsync(cancellationToken);
+
+            _context.Comments.RemoveRange(commentsToDelete);
+
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ GameStore.Application/UseCases/Comments/Commands/IsDeleteComment/IsDeletedCommentCommand.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using GameStore.Application.Common.Interfaces;
+using GameStore.Domain.Entities;
+using MediatR;
+
+namespace GameStore.Application.UseCases.Comments.Commands.IsDeleteComment
+{
+    public record IsDeletedCommentCommand(int Id) : IRequest;
+
+    public class IsDeletedCommentCommandHandler : IRequestHandler<IsDeletedCommentCommand>
+    {
+        IApplicationDbContext _dbContext;
+        IMapper _mapper;
+
+        public IsDeletedCommentCommandHandler(IApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task Handle(IsDeletedCommentCommand request, CancellationToken cancellationToken)
+        {
+            var Comment = _dbContext.Comments.Find(request.Id);
+            
+            if (!Comment.IsDeleted)
+            {
+                Comment.IsDeleted = true;
+            }
+
+            _mapper.Map(request, Comment);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+        }
+    }
+}
